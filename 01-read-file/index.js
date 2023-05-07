dcdb9dc5dfccb8d0dfc9e78a835c6a94174a4e52
shchegolenkov <denis.amorales@ya.ru>
2023-05-07 15:45:24 +0300

    feat: 01-read-file done

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(source, 'utf-8');
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
+stream.on('error', error => console.log('Error', error.message));
