619c14f9b7c621d0cac3c891811693464b53a3ec
shchegolenkov <denis.amorales@ya.ru>
2023-05-07 20:01:53 +0300

    feat: 05-merge-styles done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const input = path.join(__dirname, 'styles');
+const output = path.join(__dirname, 'project-dist');
+
+async function copyDir(inputFolder, outputFolder) {
+  const outputFile = path.join(outputFolder, 'bundle.css');
+  await fsPromises.rm(outputFile, { recursive: true, force: true });
+  const output = fs.createWriteStream(outputFile);
+
+  (await fsPromises.readdir(inputFolder)).forEach(file => {
+    if (path.extname(file) === '.css') {
+      const inputStream = fs.createReadStream(path.join(inputFolder, file), 'utf-8');
+      inputStream.on('data', data => {
+        output.write(data.toString() + '\n');
+      });
+    }
+  });
+}
+
+copyDir(input, output);
